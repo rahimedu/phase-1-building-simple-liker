9d5f90ea9219bf638a1f39f0401e97ed4d8b1046
rahimedu <edwin.duche82@gmail.com>
2022-06-16 23:50:41 -0400

    message

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener[ 'DOMContentloaded'],()=>{}
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(()=> {
+      let error = document.getElementById('modal')
+      error.classList.remove ('hidden')
+      let message = document.getElementById(modal.message)
+      message.innerText= "Random Server error.Tryagain."
+       setTimeout()= {
+        },3000
+    });
+}
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+ 
 
 
 //------------------------------------------------------------------------------
